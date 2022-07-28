8af6bc7f80b76f50cc73b07954a7bf4ce39c512f
Arman Attarzadeh <arman.atz@gmail.com>
2022-07-28 10:11:18 +0000

    Inject Google Maps script into trip page

--- src/pages/TripPage/index.jsx
+++ src/pages/TripPage/index.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Trip from "../../components/Trip";
 import TripMap from "../../components/TripMap";
+import { useLoadScript } from "@react-google-maps/api";
 import { useParams } from "react-router-dom";
 import { getTripById } from "../../api/trips.api";
 import "./style.css";
+
+const MAP_LIBRARIES = ["places"];
 
 export default function TripPage() {
     const { tripId } = useParams();
     const { trip, isLoading } = useTrip(tripId);
 
+    const { isLoaded: isGMapScriptLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_PLACES_API_KEY,
+        libraries: MAP_LIBRARIES,
+    });
+
     return (
         <>
-            {isLoading && <div>Loading...</div>}
+            {isGMapScriptLoaded && isLoading && <div>Loading...</div>}
             {trip && (
                 <div className="trip-page">
                     <div className="trip-page__sidebar">
